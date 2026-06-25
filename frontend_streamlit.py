6f1e925c02d597616df8e2c35c2d3f1e8cbc18a8
Grace <gkchan09@gmail.com>
2026-06-24 23:41:21 -0700

    Add comment on how to run streamlit frontend

--- frontend_streamlit.py
+++ frontend_streamlit.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
+# Command to start the server: streamlit run frontend_streamlit.py 
+# URL to view the frontend: http://localhost:8501
+
 st.title("FastAPI Streamlit Frontend")
