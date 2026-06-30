d40a85704f7f623b58ca8082ef82fcf2ff4b9462
Grace <gkchan09@gmail.com>
2026-06-29 23:28:50 -0700

    Add database file for creating sqlite database

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine, SQLModel
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}  # Needed for SQLite multithreading
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db()
