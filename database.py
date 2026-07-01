321c633c0f9cf34da8d8cc61808905ed2ede50b7
Grace <gkchan09@gmail.com>
2026-06-30 21:03:39 -0700

    Add function to get session

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, Session, SQLModel
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -9,6 +9,11 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def create_db():
     SQLModel.metadata.create_all(engine)
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 if __name__ == "__main__":
     create_db()
+    get_session()
