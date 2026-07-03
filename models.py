78db3e8909b58ec183128d560408ce9f1759e75a
Grace <gkchan09@gmail.com>
2026-07-02 22:17:48 -0700

    Add item table to models

--- models.py
+++ models.py
@@ -1,7 +1,13 @@
 from sqlmodel import Field, SQLModel
+from typing import Optional
 
 class User(SQLModel, table=True):
     user_id: int = Field(primary_key=True)
     username: str = Field(nullable=False)
     email: str = Field(unique=True)
-    
+
+class Item(SQLModel, table=True):
+    item_id: int = Field(primary_key=True)
+    name: str
+    description: Optional[str] = None
+    price: float
